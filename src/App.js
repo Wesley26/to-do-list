4e15e79b3a8b47b4799a80534068c0f9d9258480
Wesley <Gamer@DESKTOP-PQIIC82>
2020-08-11 00:20:08 -0400

    08/11/2020 Commit
    08/11/2020 Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
+//import React from 'react';
+import React, { useState } from 'react';
+import Todos from "./components/Todos.js";
 import './App.css';
 
+
 function App() {
+  // todos - names the state
+  // setTodos - sets the state
+  const [todos, setTodos] = useState([  //functional component
+    { text: "Plan how to create a house." },
+    { text: "Write the program for an automated house-building device." },
+    { text: "Plan how to create a todo list." },
+    { text: "Play some video games." },
+    { text: "Clean up the bedroom" },
+    { text: "Empty the trash." },
+
+  ]);
+
+  const Todo = ({ todo }) => 
+  <div className="todo">
+    {todo.text}
+  </div>
+
   return (
-    <div className="App">
-      <h1>Hello World! I am an App.</h1>
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+      </div>
     </div>
   );
 }
