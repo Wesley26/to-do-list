25145e4909d6f0ba0c958661a14fab422947804f
Wesley <Gamer@DESKTOP-PQIIC82>
2020-08-13 00:16:26 -0400

    08/13/2020 commit
    08/13/2020 commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
 
+/**
 const Todo = ({ todo }) => 
   <div className="todo">
     {todo.text}
   </div> //JSX element
+*/
 
 function TodoForm({ addTodo }) {
 
@@ -34,20 +36,53 @@ function TodoForm({ addTodo }) {
   );
 }
 
+function Todo({ todo, index, completeTodo}) {
+  return (
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      {todo.text}
+
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete Item</button>
+      </div>
+    </div> //JSX element
+  );
+}
+
 function App() {
   // todos - names the state
   // setTodos - sets the state
   const [todos, setTodos] = useState([  //functional component
-    { text: "Plan how to create a house." },
-    { text: "Write the program for an automated house-building device." },
-    { text: "Plan how to create a todo list." },
-    { text: "Play some video games." },
-    { text: "Clean up the bedroom" },
-    { text: "Empty the trash." }
+    { text: "Plan how to create a house.", //text object
+      isCompleted: false  //boolean object to determine if completed or not
+    },
+    { text: "Write the program for an automated house-building device.",
+      isCompleted: false 
+    },
+    { text: "Plan how to create a todo list.",
+      isCompleted: false 
+    },
+    { text: "Play some video games.",
+      isCompleted: false 
+    },
+    { text: "Clean up the bedroom",
+      isCompleted: false 
+    },
+    { text: "Empty the trash.",
+      isCompleted: false 
+    }
   ]);
 
-  const addTodo = text => {
+  const addTodo = text => { //adds a new todo item with text
     const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => { //completes todo item list
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
     setTodos(newTodos);
   };
 
@@ -59,9 +94,12 @@ function App() {
             key={index}
             index={index}
             todo={todo}
+            completeTodo={completeTodo}
           />
         ))}
+      <div className="todo-list">
         <TodoForm addToDo={addTodo} />
+      </div>
       </div>
     </div>
   );
